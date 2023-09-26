a7780bd3d0a038f712cb0c89daaac059be5b7b29
Olivier Proulx <oproulx8896@gmail.com>
2023-09-26 13:06:57 -0500

    added scene managment to main menu

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void LoadScene(int Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
